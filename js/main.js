0524a41859b3ab020040fbd6889d446fcb366d4d
Jason Carney <jason.jay.carney@gmail.com>
2017-03-27 23:31:53 +0930

    update task description

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
 /**
+* Do all the following with JS only
+*
 * TODO: Calculate the total for each student
 *
 * TODO: Highlight rows under the following rules (background colour):
 *
-* 85% >     #53f442
-* 75% > 84% #a4f99a
-* 60% > 74% #d6fc58
-* 50% > 59% white
-*     < 50% #f96b6b
+* HD 85% >     #53f442
+* D  75% > 84% #a4f99a
+* C  60% > 74% #d6fc58
+* P  50% > 59% white
+* F      < 50% #f96b6b
+*
+* TODO: Add a paragraph before the table with a summary of how many students recieved each grade
+*
+* TODO: Bonus, congratulate the highest scoring student in the summary paragraph
+*
+* TODO: Bonus, Add a heading for "These students need to study harder" and list those whom recieved F's
+*
 */
